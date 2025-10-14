a5a77b5c85664d48d3938fdde1567edc287be3e3
Daiviet HUYNH <daiviet.huynh@gmail.com>
2025-10-14 14:18:51 +0700

    fixing the scheduler cron

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 require('dotenv').config();
-const cron = require('node-cron');
 const axios = require('axios');
 
 const URLs = process.env.URLS.split(',');
 
-const CronExpression = {
-  EVERY_14_MINUTES: '0 */14 * * * *',
-};
-
-// Create scheduler only in non-test environments
-let scheduler;
-
-if (process.env.NODE_ENV !== 'test') {
-  scheduler = cron.schedule(CronExpression.EVERY_14_MINUTES, async () => {
-    await Promise.all(URLs.map((url) => getHealth(url)));
-  });
-  scheduler.start();
-}
-
 module.exports = {
   sum,
-  getHealth,
-  scheduler: process.env.NODE_ENV === 'test' ? { stop: () => {} } : scheduler
+  getHealth
 };
 
 async function getHealth(URL) {
